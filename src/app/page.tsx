656ca215eac8be9033bd6ec239bfbc5608ee8d97
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-10-28 01:49:34 +0530

    added work and education

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,10 +158,10 @@ export default function Page() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                  Hackathons
+                  Certifications
                 </div>
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  I like building things
+                  I like skilling up 
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   During my time in university, I attended{" "}
