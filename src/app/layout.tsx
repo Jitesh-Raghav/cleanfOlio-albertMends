cbbc2c8df8bbec8ef7965e7be6629589724f9d79
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2025-06-15 15:40:33 +0530

    added metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,20 @@ export const metadata: Metadata = {
   },
   description: DATA.description,
   openGraph: {
-    title: `${DATA.name}`,
+    title: DATA.name,
     description: DATA.description,
     url: DATA.url,
-    siteName: `${DATA.name}`,
+    siteName: DATA.name,
+    images: [`${DATA.url}/over.png`], // good for LinkedIn, Discord etc.
     locale: "en_US",
     type: "website",
+  },
+  twitter: {
+    title: DATA.name,
+    description: DATA.description,
+    card: "summary_large_image",
+    site: "@okayjitesh",
+    images: [`${DATA.url}/over.png`],
   },
   robots: {
     index: true,
@@ -38,15 +46,49 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
-  twitter: {
-    title: `${DATA.name}`,
-    card: "summary_large_image",
-  },
   verification: {
     google: "",
     yandex: "",
   },
 };
+
+
+
+// export const metadata: Metadata = {
+//   metadataBase: new URL(DATA.url),
+//   title: {
+//     default: DATA.name,
+//     template: `%s | ${DATA.name}`,
+//   },
+//   description: DATA.description,
+//   openGraph: {
+//     title: `${DATA.name}`,
+//     description: DATA.description,
+//     url: DATA.url,
+//     siteName: `${DATA.name}`,
+//     locale: "en_US",
+//     type: "website",
+//   },
+//   robots: {
+//     index: true,
+//     follow: true,
+//     googleBot: {
+//       index: true,
+//       follow: true,
+//       "max-video-preview": -1,
+//       "max-image-preview": "large",
+//       "max-snippet": -1,
+//     },
+//   },
+//   twitter: {
+//     title: `${DATA.name}`,
+//     card: "summary_large_image",
+//   },
+//   verification: {
+//     google: "",
+//     yandex: "",
+//   },
+// };
 
 export default function RootLayout({
   children,
